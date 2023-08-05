98eafc1a325afaa6f81f547c0aa9625e07e212c2
b1tburn3r20 <alejandrojosuebermudez@gmail.com>
2023-08-04 20:52:51 -0500

    updated 3js & auth

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
 import SignUpForm from "../../components/SignUpForm/SignUpForm";
 import LoginForm from "../../components/LoginForm/LoginForm";
+import Scene from "../../components/Scene/Scene";
 
 import "./AuthPage.css";
 export default function AuthPage({ setUser }) {
   const [showSignUp, setShowSignUp] = useState(false);
   return (
     <main>
-      <h1>AuthPage</h1>
+      <Scene />
       {showSignUp ? (
         <SignUpForm setUser={setUser} />
       ) : (
